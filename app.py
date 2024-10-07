ee7cf7033028b6c738941c4f9e3a354e3e968ae1
KarmaWSYD <powertuukka@gmail.com>
2024-10-07 22:31:48 +0300

    modified:   app.py

--- app.py
+++ app.py
@@ -7,8 +7,62 @@ from discord import app_commands
 import pickle
 import asyncio
 
+class Member():
+    def __init__(self, member_id):
+        self.member_id = member_id
+        self.rank = 0
+        self.organization = Organization()
+        
+    async def set_rank(self, rank):
+        self.rank = rank
+        
+    async def set_organization(self, organization):
+        self.organization = organization
+        
+    async def get_rank(self):
+        return self.rank
+    
+    async def get_organization(self):
+        return self.organization
+    
+    async def get_member_id(self):
+        return self.member_id
+
+class Organization():
+    def __init__(self):
+        self.leadership = {}
+        self.members = {}
+        self.name = ""
+        
+    async def add_member(self, member_id):
+        self.members.update({member_id: Member(member_id)})
+
+    async def add_leader(self, member_id):
+        self.leadership.update({member_id: Member(member_id)})
+
+    async def remove_member(self, member_id):
+        self.members.pop(member_id)
+        
+    async def remove_leader(self, member_id):
+        self.leadership.pop(member_id)
+        
+    async def set_name(self, name):
+        self.name = name
+
+
 class Server():
-    pass
+    def __init__(self):
+        self.organizations = {}
+
+    async def add_organization(self, organization_name):
+        self.organizations.update({organization_name: Organization()})
+
+    async def remove_organization(self, organization_name):
+        self.organizations.pop(organization_name)
+        
+    async def get_organization(self, organization_name):
+        return self.organizations[organization_name]
+    
 
 class Servers():
     def __init__(self):
@@ -16,6 +70,13 @@ class Servers():
 
     async def add_server(self, server_id):
         self.servers.update({server_id: Server()})
+        
+    async def remove_server(self, server_id):
+        self.servers.pop(server_id)
+    
+    async def get_server(self, server_id):
+        return self.servers[server_id]
+    
 
 class MyClient(discord.Client):
     def __init__(self, *, intents: discord.Intents):
